9a9af9f3261003b98fdbe1db8fd6769d8ee8f790
jocarpe <114078928+chemacarceller@users.noreply.github.com>
2026-01-11 13:38:03 +0100

    Add files via upload

--- QuickFolderSynchro.sh
+++ QuickFolderSynchro.sh
@@ -1,34 +1,41 @@
 #! /bin/bash
-# Este script sirve para actualizar un disco duro en otro
-# Se le debe pasar la ubicacion del disco Origen y del disco Destino
-# Para ello realiza los siguientes pasos :
-# 0) Crear el directorio destino si no existe
-# 1) Para cada directorio comprueba los archivos distintos de tipo directorio y si tienen contenido distinto del destino lo copia
-# El contenido distinto lo estima a partir de los tamaños y de las fechas de modificación
-# 2) Ver la lista de archivos normales y directorios distintos del Destino que no esten en Origen y eliminarlos
-# 3) Para cada directorio Origen lanzar de forma recursiva el script
+# This script is used to update one hard drive folder on another.
+# You must pass it the location of the Source and Destination disks folders.
+# To do the task, the script performs the following steps:
+# 0) Create the destination directory if it doesn't exist.
+# 1) For each directory, check for files of type other than directory. If they have content different from the destination, copy them.
+# It estimates the different content based on sizes and modification dates.
+# 2) Check the list of regular files and directories different from the Destination that are not in the Source and delete them.
+# 3) For each Source directory, recursively run the script.
 
-if test "$2" = ""; then
-   echo "Argumentos incorrectos";
+# Wrong arguments
+if test $# -ne 2; then
+   echo "Wrong arguments";
    exit 1
 fi
 
-confirmar="$3"
-if test "$confirmar" != "No"; then
-   echo -e "Confirma que $2 es correcto ? Conteste Yes para continuar No para cancelar \c"; read resp;   
+recursion=false
+# Checking Recursion
+if [ -z ${MY_COOL_SCRIPT_REC+x} ]; then
+   echo -e "Confirm that $2 is correct? Answer Yes to continue, No to cancel \c"; read resp;
    while test "$resp" != "Yes"; do 
       if test "$resp" = "No"; then
          exit 2;
       fi
-      echo -e "Confirma que $2 es correcto ? Conteste Yes para continuar No para cancelar \c"; read resp;      
+      echo -e "Confirm that $2 is correct? Answer Yes to continue, No to cancel \c"; read resp;      
    done
+else
+   recursion=true
 fi
+
+# set an env variable local to this shell
+export MY_COOL_SCRIPT_REC=1
 
 directorioOrigen="$1"
 directorioDestino="$2"
 
-#echo " Directorio ORIGEN : $1"
-#echo " Directorio DESTINO : $2"
+#echo " SOURCE Directory : $1"
+#echo " DESTINATION Directory : $2"
 
 IFS='
 '
@@ -39,53 +46,53 @@ archivosDestinoEncontrados=0;
 archivosDestinoEliminados=0;
 
 if [ ! -d "$1" ]; then
-	# No existe el directorio origen $1
-	echo "No existe el directorio destino $1"
+	# The source directory $1 does not exist.
+	echo "The source directory $1 does not exist"
 	exit 1
 fi
 
 if [ ! -d "$2" ]; then
-	# No existe el directorio destino $2
-	if test "$confirmar" != "No"; then
-		echo "No existe el directorio destino $2"
+	# The destination directory $2 does not exist.
+	if ! $recursion; then
+		echo "The destination directory $2 does not exist."
 		exit 1
 	else
-		echo "No existe el directorio destino $2, se crea"
+		echo "The destination directory $2 does not exist, it is created"
 		if ! mkdir "$2"; then
-			echo "No se ha podido crear el directorio destino $2"
+			echo "The destination directory $2 could not be created"
 			exit 1
 		fi
 	fi
 fi
 
-#echo "BUSCANDO ARCHIVOS EN $1"
-# Se buscan los archivos en origen
+#echo "SEARCHING FOR FILES IN $1"
+# The source files are being searched for
 for i in `echo "$1/*"`; do
 
-	# Si no hay archivos se sale del bucle
+	# If there are no files, the loop exits.
 	if [ "$i" = "$1/*" ]; then
-		#echo "No hay archivos en $1"
+		#echo "There are no files in $1"
 		break;
 	fi 
 	
-	# Para cada archivo ORIGEN se extrae el nombre y el tamaño y la fecha de modificación
+	# For each SOURCE file, the name, size, and modification date are extracted.
 	rutaOrigen="$i";
 	nombreOrigen=${rutaOrigen##*/}
 	tamanyoOrigen=`ls -l "$i" | cut -f5 -d" "`;
 	fechaOrigen=`stat -c %Y "$i"`;
 	
-	# Los archivos con corchetes dan problemas por lo que se sustituyen por guiones
+	# Files with square brackets cause problems, so they are replaced with hyphens.
 	nombreOrigenModificado=`echo $nombreOrigen | tr '[' '-' | tr ']' '-'`;
 	if [ "$nombreOrigen" != "$nombreOrigenModificado" ] ; then
-		echo "$1 -->  Nombre Archivo RENOVADO $nombreOrigenModificado"
+		echo "$1 -->  Renamed File $nombreOrigenModificado"
 		if ! mv "$1/$nombreOrigen" "$1/$nombreOrigenModificado"; then
-			echo "ERROR: No ha podido renombrarse $1/$nombreOrigen"
+			echo "ERROR: Could not rename $1/$nombreOrigen"
 		fi
 	fi
 	
-	# A partir de aqui no hay que utilizar $i sino $1/$nombreOrigenModificado
+	# From here on you don't need to use $i but $1/$nombreOrigenModificado
 	
-	# Si el archivo no es un directorio
+	# If the file is not a directory
 	if [ ! -d "$1/$nombreOrigenModificado" ]; then
 		archivosEncontrados=$(( archivosEncontrados + 1  ));
 
@@ -94,101 +101,97 @@ for i in `echo "$1/*"`; do
 			tamanyoDestino=`ls -l "$2/$nombreOrigenModificado" | cut -f5 -d" "`;
 			fechaDestino=`stat -c %Y "$2/$nombreOrigenModificado"`;
 			
-			# Si el archivo destino existe se comprueba si tiene el mismo contenido
+			# If the destination file exists, it is checked whether it has the same content.
 	   		#if ! cmp "$i" "$2/$nombreOrigen" >/dev/null 2>/dev/null ; then
-	   		# Se comprueba que tienen el mismo tamaño y fecha origen es anterior a fecha destino - es mas rapido que cmp
+	   		# It is verified that they have the same size and the origin date is earlier than the destination date - it is faster than cmp
 	   		if [ "$tamanyoOrigen" != "$tamanyoDestino" -o $fechaOrigen -gt $fechaDestino ]; then
-	   			# Si los archivos no tienen el mismo contenido se copia el archivo
+	   			# If the files do not have the same content, the file is copied.
 	   			archivosExistentesCopiados=$(( archivosExistentesCopiados + 1  )); 
 	   			if [ "$tamanyoOrigen" != "$tamanyoDestino" ]; then
-					echo "$1 --> $2/$nombreOrigenModificado EXISTE TAMAÑO DISTINTO, SE COPIA";
+					echo "$1 --> $2/$nombreOrigenModificado DIFFERENT SIZES AVAILABLE, COPY AVAILABLE";
 				else
-					echo "$1 --> $2/$nombreOrigenModificado EXISTE FECHA ORIGEN POSTERIOR, SE COPIA";
+					echo "$1 --> $2/$nombreOrigenModificado THERE IS A LATER MODIFICATION DATE IN SOURCE FILE, IT IS COPIED";
 				fi
 				if ! cp "$1/$nombreOrigenModificado" "$2/$nombreOrigenModificado"; then
-					echo "ERROR: No ha podido copiarse $1/$nombreOrigenModificado en $2"
+					echo "ERROR: Could not be copied $1/$nombreOrigenModificado in $2"
 				fi
 			#else
-				#echo "$1 --> $2/$nombreOrigenModificado EXISTE MISMO CONTENIDO, NO SE HACE NADA";
+				#echo "$1 --> $2/$nombreOrigenModificado THE SAME CONTENT EXISTS, NOTHING IS DONE";
 			fi
 		else
-			# Si no existe en destino se copia
+			# If it does not exist at the destination, it is copied.
 			archivosNoExistentesCopiados=$(( archivosNoExistentesCopiados + 1  ));
-			echo "$1 --> $2/$nombreOrigenModificado NO EXISTE, SE COPIA"
+			echo "$1 --> $2/$nombreOrigenModificado IT DOESN'T EXIST, IT'S COPIED"
 			if ! cp "$1/$nombreOrigenModificado" "$2/$nombreOrigenModificado"; then
-				echo "ERROR: No ha podido copiarse $1/$nombreOrigenModificado en $2"
+				echo "ERROR: Could not be copied $1/$nombreOrigenModificado in $2"
 			fi
 		fi
 	fi
 done
 
-#echo  -e "\nESTADISTICAS $1 :"
-#echo "$1 -> Archivos Encontrados : $archivosEncontrados"
-#echo "$1 -> Archivos Existentes Copiados : $archivosExistentesCopiados"
-#echo -e "$1 -> Archivos No Existentes Copiados : $archivosNoExistentesCopiados \n\n"
+#echo  -e "\nSTATISTICS $1 :"
+#echo "$1 -> Found Files : $archivosEncontrados"
+#echo "$1 -> Existing Files Copied : $archivosExistentesCopiados"
+#echo -e "$1 -> Non-Existent Files Copied : $archivosNoExistentesCopiados \n\n"
 
-# Se recorre destino para eliminar los archivos que no existan en origen
-#echo "BUSCANDO ARCHIVOS EN $2"
+# The destination is traversed to remove files that do not exist in the source.
+#echo "SEARCHING FOR FILES IN $2"
 for j in `echo "$2/*"`; do
 
-	# Caso que no hayan archivos se sale del bucle
+	# If there are no files, the loop exits.
 	if [ "$j" = "$2/*" ]; then
-		#echo "No hay archivos en $2"
+		#echo "There are no files in $2"
 		break;
 	fi
 	archivosDestinoEncontrados=$(( archivosDestinoEncontrados + 1  ));
 	
-	# Para cada archivo destino extraido el nombre
+	# For each destination file, getting the name
 	rutaDestino="$j";
 	nombreDestino=${rutaDestino##*/}
 	archivoDestinoExisteEnOrigen=false;
 	
-	# Comprobamos si existe en Origen
+	# We check if it exists in the Source
 	$(ls -l "$1/$nombreDestino" > /dev/null 2>/dev/null);
 	if [ $? -eq 0 ]; then
-		# El archivo existe en Origen
+		# The file exists in Source
 		archivoDestinoExisteEnOrigen=true
 		#if [ -d "$j" ]; then
-			#echo "$1 --> DIRECTORIO ENCONTRADO : $j EXISTE EN ORIGEN, NO SE HACE NADA"
+			#echo "$1 --> DIRECTORY FOUND: $j EXISTS IN SOURCE, NOTHING IS DONE"
 		#else
-			#echo "$1 --> ARCHIVO ENCONTRADO : $j EXISTE EN ORIGEN, NO SE HACE NADA"
+			#echo "$1 --> FILE FOUND: $j EXISTS IN SOURCE, NO ACTION IS REQUIRED"
 		#fi
 	fi
 
-	# Si no existe el archivo en Destino se elimina, independientemente que sea un directorio o un fichero
+	# If the file does not exist in the Destination, it is deleted, regardless of whether it is a directory or a file.
 	if [ $archivoDestinoExisteEnOrigen = "false" ]; then
 		archivosDestinoEliminados=$(( archivosDestinoEliminados + 1  ));
 		if [ -d "$j" ]; then
-			echo "$1 --> DIRECTORIO ENCONTRADO : $j NO EXISTE EN ORIGEN, SE ELIMINA"
+			echo "$1 --> DIRECTORY FOUND: $j DOES NOT EXIST IN SOURCE, IT IS DELETED"
 		else
-			echo "$1 --> ARCHIVO ENCONTRADO : $j NO EXISTE EN ORIGEN, SE ELIMINA"
+			echo "$1 --> FILE FOUND: $j DOES NOT EXIST IN SOURCE, IT IS DELETED"
 		fi
 		if ! rm -fr $j; then
-			echo "ERROR: No ha podido eliminarse $j de $2"
+			echo "ERROR: Could not be deleted $j from $2"
 		fi
 	fi
 done
 
-#echo -e "\nESTADISTICAS $2 :\n"
-#echo "$1 -> Archivos Destino Encontrados : $archivosDestinoEncontrados"
-#echo -e "$1 -> Archivos Destino Eliminados : $archivosDestinoEliminados\n\n"
+#echo -e "\nSTATISTICS $2 :\n"
+#echo "$1 -> Destination Files Found : $archivosDestinoEncontrados"
+#echo -e "$1 -> Destination Files Deleted : $archivosDestinoEliminados\n\n"
 
-# Recorremos el origen en busca de directorios
-# El directorio existe en Destino, ya comprobado
+# We trace the origin in search of directories
+# The directory exists in Destination, already verified
 for i in `echo "$1/*"`; do
-	# Para cada archivo extraido el nombre y el tamaño
+	# For each file, getting the name and size
 	rutaOrigen="$i";
 	nombreOrigen=${rutaOrigen##*/}
 	if [ -d "$i" ]; then
-		#echo "$1 -->  DIRECTORIO ENCONTRADO : $i"
-		# Se lanza el script para cada uno de los archivos
-		#echo "$1 --> Lanzado el script $0 para el directorio $i y el destino $2/$nombreOrigen";
-		if ! bash $0 "$i" "$2/$nombreOrigen" "No"; then
-			echo "ERROR: No ha podido lanzarse bash $0 $i $2/$nombreOrigen"
+		#echo "$1 -->  DIRECTORY FOUND : $i"
+		# The script is launched for each of the files
+		#echo "$1 --> The script $0 was launched for directory $i and destination $2/$nombreOrigen";
+		if ! $0 "$i" "$2/$nombreOrigen"; then
+			echo "ERROR: Could not be launched bash $0 $i $2/$nombreOrigen"
 		fi
 	fi 
 done
-
-
-
-
